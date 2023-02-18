90b10885ba4d500a10042e3cfd168001347bb865
meku <piangree147@gmail.com>
2023-02-18 23:29:01 +0800

    20230218 Major
    - Using circulus/sd-anireal-v2.5
    - Disable SFW filter
    - Remove autocast module

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from auth_token import key
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 import torch
-from torch import autocast
 from diffusers import StableDiffusionPipeline
 from io import BytesIO
 import base64 
@@ -17,15 +16,29 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+# Anime style
+# - circulus/sd-anireal-v2.5
+# - andite/anything-v4.0
+# - aipicasso/cool-japan-diffusion-2-1-0
+# - johnslegers/epic-diffusion
+# - hakurei/waifu-diffusion
+# - nitrosocke/Arcane-Diffusion
+
+# Non-anime style
+# - nitrosocke/Nitro-Diffusion (Disney-ish)
+# - dreamlike-art/dreamlike-photoreal-2.0
+# - nitrosocke/Future-Diffusion ("future style ...")
+# - johnslegers/epic-diffusion
+
 device = "cuda"
-model_id = "CompVis/stable-diffusion-v1-4"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, revision="fp16", torch_dtype=torch.float16, use_auth_token=key)
+model_id = "circulus/sd-anireal-v2.5"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, use_auth_token=key)
 pipe.to(device)
+pipe.safety_checker = None
 
 @app.get("/")
-def generate(prompt: str): 
-    with autocast(device): 
-        image = pipe(prompt, guidance_scale=8.5).images[0]
+def generate(prompt: str):
+    image = pipe(prompt).images[0]
 
     buffer = BytesIO()
     image.save(buffer, format="PNG")
